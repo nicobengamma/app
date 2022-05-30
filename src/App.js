90af67d69c36237fa8292ca7d0fe7913e08a8526
nicobengamma <kikecarc17@gmail.com>
2022-05-30 11:41:28 -0300

    navBarBootstrap

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
         </a>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
       </header>
     </div>
   );
