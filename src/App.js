f8e70aea205e22ea772902f3f54ce60a996a3347
nicobengamma <kikecarc17@gmail.com>
2022-05-30 11:36:37 -0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Componentes/NavBar';
 
+
 function App() {
   return (
     <div className="App">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></link>
+      <NavBar/>
       <header className="App-header">
-        <NavBar/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
--- src/Componentes/NavBar.js
+++ src/Componentes/NavBar.js
@@ -1,14 +1,29 @@
 const NavBar = () => {
     return (
-      <nav style={{ display: "flex" }}>
-        <div>
-          <button>Celulares</button>
-          <button>Smart TV</button>
-          <button>Notebook</button>
-          <button>P.C</button>
-          <button>Electrodomesticos</button>
-        </div>
-      </nav>
+      <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">% Off</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">SmartPhone's</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Notebook's</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Smart TV's</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     );
   };
   
