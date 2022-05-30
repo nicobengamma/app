2776a390305db4bfb8b7b9b11c3131cae921acd6
nicobengamma <kikecarc17@gmail.com>
2022-05-30 11:01:33 -0300

    arreglando navbar

---
+++ src/Componentes/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return (
+      <nav style={{ display: "flex" }}>
+        <h3>E-Comercce</h3>
+        <div>
+          <button>Celulares</button>
+          <button>Smart TV</button>
+          <button>Notebook</button>
+          <button>P.C</button>
+          <button>Electrodomesticos</button>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default NavBar;
