5835c539919770bef299f3ae016075630e24f4b5
Nicolas DiMarco <kikecarc17@gmail.com>
2022-05-30 22:39:07 -0300

    catalogo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './Componentes/NavBar';
-import ItemListContainer from './Componentes/ItemListContainer/ItemListContainer'
-
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./Componentes/NavBar";
+import ItemListContainer from "./Componentes/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <div className="App">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></link>
-      <NavBar/>
-      <ItemListContainer titulo="Comunity"/>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+        crossorigin="anonymous"
+      ></link>
+      <NavBar />
+      <ItemListContainer titulo="Comunity" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+          crossorigin="anonymous"
+        ></script>
       </header>
     </div>
   );
--- src/Componentes/NavBar.js
+++ src/Componentes/NavBar.js
@@ -1,33 +1,55 @@
-import logo from './img/logo.svg'
+import logo from "./img/logo.svg";
 
 const NavBar = () => {
-    return (
-      <nav class="navbar navbar-expand-lg bg-primary px-4">
-  <div class="container-fluid">
-  <img src={logo} alt="" width="40" height="40" class="d-inline-block align-text-top"/>
-    <a class="navbar-brand" href="#"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">% Off</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">SmartPhone's</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Notebook's</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Smart TV's</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    );
-  };
-  
-  export default NavBar;
+  return (
+    <nav class="navbar navbar-expand-lg bg-primary px-4">
+      <div class="container-fluid">
+        <img
+          src={logo}
+          alt=""
+          width="40"
+          height="40"
+          class="d-inline-block align-text-top"
+        />
+        <a class="navbar-brand" href="#"></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                % Off
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                SmartPhone's
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Notebook's
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Smart TV's
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
